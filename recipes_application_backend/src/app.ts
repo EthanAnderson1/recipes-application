d55ae4d3088296ae106a6e9fb55e1fc76cbebcdd
Ethan Anderson <ethan.anderson188@gmail.com>
2025-09-04 03:05:26 +0100

    added logging in console

--- recipes_application_backend/src/app.ts
+++ recipes_application_backend/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import routes from './routes.ts';
 import cors from 'cors';
-//import Logger from './middleware/Logger';
+import morgan from 'morgan';
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-//app.use(Logger);
+app.use(morgan('dev'));
 app.use('/', routes);
 
 export default app;
