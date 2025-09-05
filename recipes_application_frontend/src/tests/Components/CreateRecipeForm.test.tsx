4e53fe82edf2d1513caf0e2ef47e978a8aa66fee
Ethan Anderson <ethan.anderson188@gmail.com>
2025-09-05 12:47:52 +0100

    clean up create recipe test code

--- recipes_application_frontend/src/tests/Components/CreateRecipeForm.test.tsx
+++ recipes_application_frontend/src/tests/Components/CreateRecipeForm.test.tsx
@@ -5,26 +5,32 @@ import "@testing-library/jest-dom"
 describe('CreateRecipeForm test', () => {
     describe('create recipe form renders', ()=>{
       it('should renders form fields', () => {
-          render(<RecipeForm />);
-          expect(screen.getByLabelText("Title")).toBeInTheDocument();
-          expect(screen.getByLabelText("Cooking Time (minutes)")).toBeInTheDocument();
-          expect(screen.getByLabelText("Ingredients (comma separated)")).toBeInTheDocument();
-          expect(screen.getByLabelText("Instructions")).toBeInTheDocument();
+        //Arrange
+        render(<RecipeForm />);
+        //Assert
+        expect(screen.getByLabelText("Title")).toBeInTheDocument();
+        expect(screen.getByLabelText("Cooking Time (minutes)")).toBeInTheDocument();
+        expect(screen.getByLabelText("Ingredients (comma separated)")).toBeInTheDocument();
+        expect(screen.getByLabelText("Instructions")).toBeInTheDocument();
       });
     });
     
     describe('create recipe form renders', ()=>{
-      it('calls onSubmit when form is submitted', () => {
-      
+      it('should call onSubmit when form is submitted', () => {
+        //Arrange
         const handleSubmit = jest.fn();
         render(<RecipeForm />);
+        screen.getByTestId('createRecipeForm').onsubmit = handleSubmit;
+
+        //Act
         fireEvent.change(screen.getByLabelText("Title"), { target: { value: 'Food' } });
         fireEvent.change(screen.getByLabelText("Cooking Time (minutes)"), { target: { value: 5 } });
         fireEvent.change(screen.getByLabelText("Ingredients (comma separated)"), { target: { value: 'Food' } });
         fireEvent.change(screen.getByLabelText("Instructions"), { target: { value: 'Food' } });
-        screen.getByTestId('createRecipeForm').onsubmit = handleSubmit;
         const button = screen.getAllByText('Create Recipe').filter((el) => el.tagName === 'BUTTON')[0];
         fireEvent.click(button);
+
+        //Asset
         expect(handleSubmit).toHaveBeenCalled();
       });
     });
