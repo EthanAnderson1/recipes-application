c0247ff07a8618b97ceb62ee8be3a8945f702858
Ethan Anderson <ethan.anderson188@gmail.com>
2025-09-05 02:20:43 +0100

    implement get favourite and your own recipes

--- recipes_application_frontend/src/components/FavouriteRecipes.tsx
+++ recipes_application_frontend/src/components/FavouriteRecipes.tsx
@@ -1,9 +1,34 @@
-export const FavouriteRecipes = () => {
-    return (
+import { useEffect, useState } from "react";
+import {api} from "../services/API.ts";
+import{ Row} from "react-bootstrap";
+import { RecipeCard } from "./RecipeCard.tsx";
 
+export const FavouriteRecipes = () => {
+
+    const [recipes, setRecipes] = useState([]);
+    
+    useEffect(() => {
+        const fetchFavouriteRecipes = async () => {
+            const token = localStorage.getItem("token");
+            try {
+                const res = await api.get(`/favouriteRecipes`, {
+                headers: { Authorization: `Bearer ${token}` }
+                });
+                setRecipes(res.data);
+            } catch (err) {
+                console.error("Failed to fetch recipes", err);
+            }
+        };
+        fetchFavouriteRecipes();
+      }, []);
+    return (
         <div>
             <h2>Favourite Recipes</h2>
-            <p>List of favourite recipes will be displayed here.</p>
+            {recipes.map((r) => (
+                <Row key={r.id}>
+                    <RecipeCard prop={r}/>
+                </Row>
+            ))}
         </div>
     );
 }
--- recipes_application_frontend/src/components/MyRecipes.tsx
+++ recipes_application_frontend/src/components/MyRecipes.tsx
@@ -1,8 +1,37 @@
+import { useContext, useEffect, useState } from "react";
+import {api} from "../services/API.ts";
+import { UserContext } from "../services/UserContext.tsx";
+import{ Row} from "react-bootstrap";
+import { RecipeCard } from "./RecipeCard.tsx";
+
 export const MyRecipes = () => {
+
+    const token = localStorage.getItem("token");
+    const [user, setUser] = useContext(UserContext);
+    const [recipes, setRecipes] = useState([]);
+    
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const res = await api.get(`/recipesByUsername/${user}`, {
+                    headers: { Authorization: `Bearer ${token}` }
+                });
+                setRecipes(res.data);
+            } catch (err) {
+                console.error("Failed to fetch recipes", err);
+            }
+        }
+        fetchRecipes();
+      }, []);
+    
     return (
         <div>
             <h2>My Recipes</h2>
-            <p>List of my recipes will be displayed here.</p>
+            {recipes.map((r) => (
+                    <Row key={r.id}>
+                        <RecipeCard prop={r}/>
+                    </Row>
+            ))}
         </div>
     );
 }
