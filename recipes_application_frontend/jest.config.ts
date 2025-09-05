e1fd617ff2fbc98406400aa2c714c624fe72835d
Ethan Anderson <ethan.anderson188@gmail.com>
2025-09-05 11:53:46 +0100

    set up frontend testing

---
+++ recipes_application_frontend/jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom"]
+};
